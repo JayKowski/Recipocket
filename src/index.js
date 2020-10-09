a7dca25eff9ae491cbd64921477ded10294b62b3
JayKowski <jaymaina5@gmail.org>
2020-10-09 23:37:25 +0300

    connected the app to the redux store

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './stylesheets/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/reducers'
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
