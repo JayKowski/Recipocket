e7a13d7108985209e9175386e6e0515b43687f71
JayKowski <jaymaina5@gmail.org>
2020-10-09 23:32:51 +0300

    Connected main component to store, maped dispatch and state to props

--- src/components/App.js
+++ src/components/App.js
@@ -4,21 +4,38 @@ import Meal from './Meal';
 import MealSearch from './MealSearch';
 import MealPreview from './MealPreview';
 import CategorySearch from './CategorySearch';
+import { bindActionCreators } from 'redux';
+import * as actions from '../actions/actions'
+import { connect } from 'react-redux';
 
 
-function App() {
-  return (
-    <div>
-      <hr />
-      <h1>Meal Tips</h1>
-      <p>Your number one place for delicious meal recipes</p>
-      <hr />
-      <MealSearch />
-      <MealPreview />
-      <Meal />
-      <CategorySearch />
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    console.log(this.props.state);
+    return (
+      <div>
+        <hr />
+        <h1>Meal Tips</h1>
+        <p>Your number one place for delicious meal recipes</p>
+        <hr />
+        <MealSearch addMeal={this.props.addMeal} meal={this.props.state.oneMealReducer} />
+        <MealPreview meal={this.props.state.oneMealReducer} />
+        <Meal />
+        <CategorySearch />
+      </div>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    state,
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ ...actions }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
