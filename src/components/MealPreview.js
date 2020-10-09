ec1959895bf256d032dba037e549890817c3b112
JayKowski <jaymaina5@gmail.org>
2020-10-09 23:32:17 +0300

    connected Meal preview to store

--- src/components/MealPreview.js
+++ src/components/MealPreview.js
@@ -1,14 +1,30 @@
 import React from 'react';
 
 class MealPreview extends React.Component {
-    render() {
-        return (
+  render() {
+    console.log(this.props.meal);
+    if (this.props.meal) {
+      return (
+        <div>
+          <h2>_Meal Preview</h2>
           <div>
-            <h2>Meal Preview Component</h2>
-            <p>this is the meal preview component</p><hr />
+            <img
+              src={this.props.meal.strMealThumb}
+              width="50"
+              height="50"
+              alt=""
+            />
           </div>
-        );
+          <ul>
+            <li>Name: {this.props.meal.strMeal}</li>
+            <li>Common in: {this.props.meal.strArea}</li>
+            <li>category: {this.props.meal.strCategory}</li>
+            <li>Tags: {this.props.meal.strTags}</li>
+          </ul>
+        </div>
+      );
     }
+  }
 }
 
 export default MealPreview; 
