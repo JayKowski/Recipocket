86d1929ffa790b0d609ca7347a3f77606237c052
JayKowski <jaymaina5@gmail.org>
2020-10-12 23:03:00 +0300

    completed Meal preview component

--- src/components/MealPreview.js
+++ src/components/MealPreview.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 class MealPreview extends React.Component {
   render() {
-    console.log(this.props.meal);
     if (this.props.meal) {
       return (
         <div>
@@ -17,10 +16,28 @@ class MealPreview extends React.Component {
           </div>
           <ul>
             <li>Name: {this.props.meal.strMeal}</li>
-            <li>Common in: {this.props.meal.strArea}</li>
+            <li>Origin: {this.props.meal.strArea}</li>
             <li>category: {this.props.meal.strCategory}</li>
             <li>Tags: {this.props.meal.strTags}</li>
           </ul>
+        </div>
+      );
+    } else if (this.props.meals) {
+      return (
+        <div>
+          <h2>Meal Preview</h2>
+          <div>
+            <img
+              src={this.props.meals.strMealThumb}
+              width="50"
+              height="50"
+              alt=""
+            />
+          </div>
+          <ul>
+            <li>Name: {this.props.meals.strMeal}</li>
+          </ul>
+          <button>See More</button>
         </div>
       );
     }
