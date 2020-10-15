3758cfd2c2bb49ce5cc2369797288ad6721d80a8
JayKowski <jaymaina5@gmail.org>
2020-10-15 14:31:03 +0300

    added basic styling

--- src/components/CategorySearch.js
+++ src/components/CategorySearch.js
@@ -1,13 +1,78 @@
 import React from 'react';
+import RenderCategs from './RenderCategs';
+
+const categoriesA = ["Beef", "Chicken", "Dessert", "Lamb", "Miscellaneous", "Pasta", "Pork", "Seafood", "Side", "Starter", "Vegan", "Vegetarian", "Breakfast", "Goat"];
+let meals;
 
 class CategorySearch extends React.Component {
-    render() {
+  constructor() {
+    super()
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidUpdate(prevState) {
+    if(prevState.state.categoryReducer !== this.props.state.categoryReducer){
+      const apiUrl = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${this.props.state.categoryReducer}`;
+      fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('...updated')
+      });
+      }
+  }
+
+  handleChange(e) {
+    this.props.changeCategory(e.target.value);
+  }
+
+  render() {
+    if(typeof this.props.state.categoryReducer === 'object') {
         return (
-            <div>
-                <h2>_Category Search</h2>
-            </div>
-        )
+        <div>
+          <h2>Category Search</h2>
+          <form>
+            <label>
+              Choose a category to search by :
+              <select onChange={this.handleChange}>
+                {categoriesA.map((categ, index) => (
+                  
+                  <option
+                    value={`${categ}`}
+                    key={`${index}`}
+                  >{`${categ}`}</option>
+                ))}
+              </select>
+            </label>
+          </form>
+        </div>
+      );
+    } else {
+      return (
+      <div>
+        <h2>Category Search</h2>
+        <form>
+          <label>
+            Choose a category to search by:
+            <select onChange={this.handleChange}>
+              {categoriesA.map((categ, index) => (
+                
+                <option
+                  value={`${categ}`}
+                  key={`${index}`}
+                >{`${categ}`}</option>
+              ))}
+            </select>
+          </label>
+        </form>
+        <div>
+          {
+            <RenderCategs {...this.props} category={this.props.state.categoryReducer} meals={meals}/>
+          }
+        </div>
+      </div>
+    );
     }
+  }
 }
 
 export default CategorySearch;
